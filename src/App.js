d095640bab0dd4419f1665b02b4712089a668143
Zhengyi Tian <tzyhuster@gmail.com>
2019-04-14 15:22:48 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person.js';  
 
 class App extends Component {
-  render() {
+	state = {
+		person: [
+			{ name: 'Max', age: '28'},
+			{ name: 'Manu', age: '29'},
+			{ name: 'Stephanie', age: '31'}
+		],
+		showPersonPane: false
+	}
+  
+  	switchNameHandler = () => {
+  		//console.log("Was Clicked!!")
+  		this.setState({
+  			person: [
+				{ name: 'sub', age: '1'},
+				{ name: 'Manu', age: '1'},
+				{ name: 'Stephanie', age: '1'}
+			]
+  		});
+  	}
+
+    togglePersonHandler = ()  => {
+  		const showPersonPane = this.state.showPersonPane;
+  		this.setState({
+  			showPersonPane : !showPersonPane
+  		})
+  	}
+
+  	deletePersonHandler = (personIndex) => {
+  		const person = [...this.state.person]; 
+
+  		person.splice(personIndex, 1);
+
+  		this.setState({
+  			person: person
+  		});
+  	}
+
+ 	render() {
+ 		const style = {
+ 			background: 'green',
+ 			color: 'white',
+ 			font: 'inherit',
+ 			padding: '8px',
+ 			cursor: 'pointer'
+ 		};
+
+ 		let persons = null;	
+
+  		if (this.state.showPersonPane) {
+  			persons = (
+  				<div>
+      				{
+      					this.state.person.map((a, index) => {
+      						return <Person click={() => this.deletePersonHandler(index)} name={a.name} age={a.age}/>
+      					})
+      				}
+      			</div>
+  			);
+  			style.background = 'red';
+  		}
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      	<button 
+      		style={style}
+      		onClick={this.togglePersonHandler}>toggle person
+      	</button>	
+      	{persons}
       </div>
     );
   }
 }
 
 export default App;
+
+ 
