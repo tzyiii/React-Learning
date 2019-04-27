c60a1f6db3e054e1c31b6169e721f4faf4f75a11
tzyiii <zhengyt@uci.edu>
2019-04-27 17:33:24 -0500

    use react component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,11 +4,11 @@ import Aux from '../hoc/Aux';
 
 const person = (props) => {
 	return (
-		<Aux className="Person">
+		<React.Fragment>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
-        </Aux>
+        </React.Fragment>
 	)
 };
 
